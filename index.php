03d61828192f7d056a20027c8a02a124ba72c343
Logan Butler <logan@terum.us>
2015-06-13 23:42:24 -0400

    Added
    Added my project "PAP", analylizes passwords and displays a result.
    Caution: may make you break your computer.

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!doctype html>
+<html>
+	<head>
+		<title>Passive Aggressive Passwords</title>
+
+		<!-- CSS -->
+		<link href="css/style.css" rel="stylesheet">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script src="js/lib/complexify.banlist.js"></script>
+		<script src="js/lib/complexify.js"></script>
+
+	</head>
+
+	<body>
+		<div id="wrap">
+			<div id="main" class="clearfix">
+				<section class="password">
+					<input type="password" placeholder="Please Enter a Password">
+				</section>
+				<section class="comment">
+					<span class="smiley">:)</span>
+					<span class="passive-agressive">
+						Imagine your <span class='red'> in-laws </span> are helping you with a password... this is what they'd say.
+					</span>
+				</section>
+			</div>
+		</div>
+
+		<!-- Initiate -->
+		<script src="js/passive_passwords.js"></script>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	</body>
+</html>
